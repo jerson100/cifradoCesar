dbcbba488604f36f049f9a81f6164cfacb04e541
jerson100 <juamkoo@gmail.com>
2020-08-25 19:44:26 -0500

    commit thwo

--- web/assets/js/cifradoCesar.js
+++ web/assets/js/cifradoCesar.js
@@ -10,6 +10,7 @@ btnHallar.addEventListener('click',handlerClick);
 
 const cifrarC = (message) => {
     //message = Hola.
+    message = message.toLowerCase();
     const messageSplit = message.split("");
     let response = "";
     //messageSplit = ["h","o","l","a"]
@@ -17,10 +18,23 @@ const cifrarC = (message) => {
         //h -> k  i = 0
         //o -> r  i = 1
         const index = abecedario.indexOf(messageSplit[i]);
-        response += nextCharacter(index);
+        response += index===-1?messageSplit[i]:nextCharacter2(index);
     }
     console.log(response);
     return response;
+};
+
+const nextCharacter2 = (index) => {
+    //abecedario = a b c d = 4
+    //cifrar = b
+    //index = 1
+    //salto = 3
+    const newIndex = index + salto;//4
+    if(newIndex < abecedario.length){
+        return abecedario[newIndex];
+    }
+    const diff =  newIndex - abecedario.length;
+    return abecedario[diff];
 };
 
 const nextCharacter = (index) => {
